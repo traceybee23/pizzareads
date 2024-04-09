b8152d163dc5851217ab37ae150f1c63364f3001
traceybee23 <tracebee23@gmail.com>
2024-04-09 14:36:26 -0500

    Restore the XSRF-TOKEN cookie completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
