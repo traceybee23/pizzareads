79c371ec583c7d10f9ba132d6686a5645f5b1445
traceybee23 <tracebee23@gmail.com>
2024-04-11 18:50:34 -0500

    get book by id api route complete

--- backend/routes/api/books.js
+++ backend/routes/api/books.js
@@ -4,6 +4,23 @@ const { Books } = require("../../db/models");
 
 const router = express.Router();
 
+router.get('/:bookId', async (req, res, next) => {
+
+  const book = await Books.findOne({
+    where: { id: req.params.bookId }
+  })
+
+  if (!book) {
+    const err = Error('Book not found');
+    err.message = "Book couldn't be found";
+    err.status = 404;
+    return next(err)
+  } else {
+    const bookData = book.toJSON()
+
+    res.json(bookData)
+  }
+})
 
 router.get('/', async (req, res, next) => {
 
