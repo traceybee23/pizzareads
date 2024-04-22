0719ef4f59c8366ea18bfe0ebf5a3f0e0f382b90
traceybee23 <tracebee23@gmail.com>
2024-04-22 18:53:31 -0500

    fixed message if you have hit milestone 2 and got a coupon

--- frontend/src/components/BookProgress/GoalProgress.jsx
+++ frontend/src/components/BookProgress/GoalProgress.jsx
@@ -37,27 +37,27 @@ const GoalProgress = () => {
 
   return (
     <div className='goal-progress-container'>
-      { (count < 5) && milestone(count) !== 1 && coupons.length === 0 && user.milestone === null &&
+      {(count < 5) && milestone(count) !== 1 && coupons.length === 0 && user.milestone === null &&
         <h2 className='goal-progress-banner'>
           You are {milestone(count)} books away from a free pizza!
         </h2>
       }
-      { !coupwithnoredeemdate && milestone(count) === 1 && coupons.length === 0 && user.milestone === null &&
+      {!coupwithnoredeemdate && milestone(count) === 1 && coupons.length === 0 && user.milestone === null &&
         <h2 className='goal-progress-banner'>
           You are {milestone(count)} book away from a free pizza!
         </h2>
       }
       {count === 5 && !coupwithnoredeemdate && coupons.length === 0 && user.milestone === null &&
         <div className="get-pizza-butt">
-          <AvailableCouponButton coupons={coupons}/>
+          <AvailableCouponButton coupons={coupons} />
         </div>
       }
       {count === 5 && coupwithnoredeemdate && coupons.length === 0 &&
         <Link to={'/coupons/current'} className='redeem-coupon-link'>
-          please redeem your first coupon
+          please redeem your existing coupon
         </Link>
       }
-      { (count >= 5 && count < 10) && milestone(count) !== 1 && user.milestone === 1 &&
+      {(count >= 5 && count < 10) && milestone(count) !== 1 && user.milestone === 1 &&
         <h2 className='goal-progress-banner'>
           You are {milestone(count)} books away from a free pizza!
         </h2>
@@ -65,30 +65,26 @@ const GoalProgress = () => {
       {
         (count >= 5 && count < 10) && milestone(count) === 1 && coupwithnoredeemdate && user.milestone === 1 &&
         <Link to={'/coupons/current'} className='redeem-coupon-link'>
-          please redeem your first coupon
+          please redeem your existing coupon
         </Link>
       }
-      { !coupwithnoredeemdate && (count >= 5 && count < 10) && milestone(count) === 1 && user.milestone === 1 &&
+      {!coupwithnoredeemdate && (count >= 5 && count < 10) && milestone(count) === 1 && user.milestone === 1 &&
         <h2 className='goal-progress-banner'>
           You are {milestone(count)} book away from a free pizza!
         </h2>
-      }
-      {
-        count === 10 && coupwithnoredeemdate && coupons.length === 1 && user.milestone === 1 &&
-        <Link to={'/coupons/current'} className='redeem-coupon-link'>
-          please redeem your first coupon
-        </Link>
       }
       {count === 10 && !coupwithnoredeemdate && coupons.length === 1 && user.milestone === 1 &&
         <div className="get-pizza-butt">
           <AvailableCouponButton coupons={coupons} />
         </div>
       }
-      {count === 10 && coupwithnoredeemdate && 
+      {
+        count === 10 && coupwithnoredeemdate && user.milestone === 2 &&
         <Link to={'/coupons/current'} className='redeem-coupon-link'>
-          please redeem your coupon
+          please redeem your existing coupon
         </Link>
       }
+
     </div>
   );
 };
