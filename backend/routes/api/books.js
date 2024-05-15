950b948beefaf8b0a1a2b9b32ce09645164de1e6
traceybee23 <tracebee23@gmail.com>
2024-05-15 14:09:53 -0500

    forgot the api route so i built that

--- backend/routes/api/books.js
+++ backend/routes/api/books.js
@@ -1,13 +1,55 @@
 const express = require('express');
 
-const { Books } = require("../../db/models");
+const { Books, Review, User } = require("../../db/models");
+
 
 const router = express.Router();
+
+router.get('/:bookId/reviews', async (req, res, next) => {
+
+  const book = await Books.findByPk(req.params.bookId)
+
+  if (!book) {
+    return res.status(404).json({
+      message: "Book couldn't be found"
+    })
+  }
+
+  const reviews = await Review.findAll({
+    where: { bookId: req.params.bookId },
+    include: [
+      {
+        model: User,
+        attributes: ['id', 'username']
+      }
+    ],
+    order: [['createdAt', 'DESC']]
+  })
+
+  let reviewList = [];
+
+  reviews.forEach(review => {
+    reviewList.push(review.toJSON())
+  })
+
+  if (!reviewList.length) {
+    res.json({ Reviews: "Be the first to write a review"})
+  }
+
+  res.json(reviewList)
+
+})
 
 router.get('/:bookId', async (req, res, next) => {
 
   const book = await Books.findOne({
-    where: { id: req.params.bookId }
+    where: { id: req.params.bookId },
+    include: [
+      {
+        model: Review,
+        attributes: ['stars']
+      }
+    ]
   })
 
   if (!book) {
@@ -17,6 +59,26 @@ router.get('/:bookId', async (req, res, next) => {
     return next(err)
   } else {
     const bookData = book.toJSON()
+
+    let stars = 0
+    bookData.Reviews.forEach(review => {
+      stars += review.stars
+      bookData.numReviews = bookData.Reviews.length
+      if (book.Reviews.length > 1) {
+        bookData.avgStarRating = ( stars / bookData.Reviews.length).toFixed(1)
+      } else {
+        bookData.avgStarRating = review.stars.toFixed(1)
+      }
+    })
+
+    if (!bookData.numReviews) {
+      bookData.numReviews = null
+    }
+    if (!bookData.avgStarRating) {
+      bookData.avgStarRating = "New"
+    }
+
+    delete bookData.Reviews
 
     res.json(bookData)
   }
---
+++ frontend/src/components/Reviews/Reviews.jsx
@@ -0,0 +1,15 @@
+
+import './Reviews.css'
+
+const Reviews = () => {
+
+  return (
+    <>
+    Reviews
+    </>
+  )
+
+}
+
+
+export default Reviews;
