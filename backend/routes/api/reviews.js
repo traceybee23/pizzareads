7ddd196f287cd531cf09bf2d48aaa0bfa1bd21f5
traceybee23 <tracebee23@gmail.com>
2024-05-17 14:18:05 -0500

    update review route done not tested

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { requireAuth } = require('../../utils/auth');
+const { Book, Review, User } = require('../../db/models');
+
+const router = express.Router();
+
+router.put('/:reviewId', requireAuth, async (req, res, next) => {
+  const user = req;
+  const reviewId = Number(req.params.reviewId)
+
+  try {
+    const { review, stars } = req.body;
+
+    const editReview = await Review.findOne({
+      where: {
+        id: reviewId
+      }
+    })
+
+    if (!editReview) {
+      return res.status(404).json({
+        message: "Review couldn't be found"
+      })
+    }
+
+    if (!user) {
+      return res.status(401).json({
+        "message": "Authentication required"
+      })
+    }
+
+    if (user.id !== editReview.userId) {
+      return res.status(403).json({
+        "message": "Forbidden"
+      })
+    }
+
+    if (user) {
+      editReview.set({ review, stars });
+
+      await editReview.save();
+
+      res.status(200).json(editReview);
+    }
+
+  } catch (error) {
+    error.message = "Bad Request"
+    error.status = 400
+    next(error)
+  }
+})
