e420bf6a9747c41b11e132025a75388c4ae202f0
traceybee23 <tracebee23@gmail.com>
2024-05-17 15:20:45 -0500

    delete route completed

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,7 +5,7 @@ const { Book, Review, User } = require('../../db/models');
 const router = express.Router();
 
 router.put('/:reviewId', requireAuth, async (req, res, next) => {
-  const { user }= req;
+  const { user } = req;
   const reviewId = Number(req.params.reviewId)
 
   try {
@@ -41,6 +41,48 @@ router.put('/:reviewId', requireAuth, async (req, res, next) => {
       await editReview.save();
 
       res.status(200).json(editReview);
+    }
+
+  } catch (error) {
+    error.message = "Bad Request"
+    error.status = 400
+    next(error)
+  }
+})
+
+router.delete('/:reviewId', requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const reviewId = req.params.reviewId
+
+  try {
+    const review = await Review.findOne({
+      where: {
+        id: reviewId
+      }
+    })
+    if (!review) {
+      return res.status(404).json({
+        "message": "Review couldn't be found"
+      })
+    }
+    if (!user) {
+      return res.status(401).json({
+        "message": "Authentication required"
+      })
+    }
+    if (user.id !== review.userId) {
+      return res.status(403).json({
+        "message": "Forbidden"
+      })
+    }
+
+    if (user) {
+
+      await review.destroy(review)
+
+      res.status(200).json({
+        "message": "Successfully deleted"
+      })
     }
 
   } catch (error) {
