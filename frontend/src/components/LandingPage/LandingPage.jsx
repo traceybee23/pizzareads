ced1d09b388ecf09f2e20dc834b9f66b47448fc5
traceybee23 <tracebee23@gmail.com>
2024-06-07 11:17:20 -0500

    mouseover parrallax"

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,34 @@
+import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import BookProgress from "../BookProgress/BookProgress"
-import './LandingPage.css'
-
+import './LandingPage.css';
 
 const LandingPage = () => {
+  const user = useSelector(state => state.session.user);
+  const titleRef = useRef(null);
 
-  const user = useSelector(state => state.session.user)
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (titleRef.current) {
+        const { clientX, clientY } = e;
+
+        const xPos = (clientX / window.innerWidth - 0.5) * 150;
+        const yPos = (clientY / window.innerHeight - 0.5) * 150;
+        titleRef.current.style.backgroundPosition = `${xPos}px ${yPos}px`;
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
   return (
     <div className='landing-page'>
       {!user &&
         <>
-          <h1 className='title'>pizzareads</h1>
-
+          <h1 className='title' ref={titleRef}>pizzareads</h1>
         </>
       }
       {user &&
@@ -21,7 +37,7 @@ const LandingPage = () => {
         </div>
       }
     </div>
-  )
-}
+  );
+};
 
 export default LandingPage;
