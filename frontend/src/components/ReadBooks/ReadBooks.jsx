f4aa4d175062251dffd4fec81841c4b13d19b754
traceybee23 <tracebee23@gmail.com>
2024-06-10 20:23:14 -0500

    fixed message if there is no book trophies

--- frontend/src/components/ReadBooks/ReadBooks.jsx
+++ frontend/src/components/ReadBooks/ReadBooks.jsx
@@ -25,7 +25,7 @@ const ReadBooks = () => {
         book trophies
       </h1>
       <div className="book-progress-completed">
-        {user && progresses && progresses.map(progress => (
+        {user && progresses.length ? (progresses.map(progress => (
           progress.completed === true &&
           <div
             className="book-read-cards"
@@ -35,7 +35,9 @@ const ReadBooks = () => {
               <img className="read-image" src={progress.coverImageUrl} onClick={() => navigate(`/books/${progress.bookId}`)} />
             </>
           </div>
-        ))}
+        ))): (
+          <div>the books you complete will be shown here</div>
+        )}
       </div>
     </div>
   )
